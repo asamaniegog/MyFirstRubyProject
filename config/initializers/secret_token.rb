4af599a9f738453d871b233ee4df79bfee38a346
Alex Samaniego Guillen <asamaniegog@avantica.net>
2011-02-21 18:29:11 -0500

    Initial commit - First rubty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstRubyProject::Application.config.secret_token = '1a8ca9eb1b9293862416c6578d5f5e0607bf806aabbe3ec026cac9ffaa0ec920733904990aef60de3f0bd841fc951a05f9618afaec50f826dfd8c371a6238579'
